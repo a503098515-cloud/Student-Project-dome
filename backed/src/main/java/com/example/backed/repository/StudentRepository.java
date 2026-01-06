916224f6668f8b523efaa40bfa618dce3b4c6aaf
a503098515-cloud <a503098515@gmail.com>
2026-01-06 02:26:02 -0500

    Delete backed/src/main/java/com/example/backed/repository/StudentRepository.java

--- backed/src/main/java/com/example/backed/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backed.repository;
-
-import com.example.backed.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository; 
-import org.springframework.stereotype.Repository;
-
-@Repository//Handles database operations
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
